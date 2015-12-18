a2eb9572352f7207a518eb547a5e19bf4ba65c49
Mohit Joshi <animusmojo@gmail.com>
2015-12-18 15:15:22 +0530

    Fully functional clock

--- main.js
+++ main.js
@@ -1,62 +1,84 @@
 $(document).ready(function(){
 	//globals
-	var timeLeft = parseInt($("#sessionText").text()) * 60;
+	var timeLeft = parseInt($("#sessionText").text()) * 60 ; // this varible stores the time in seconds
 	var sessionRunning = true;
 	var breakRunning = false;
-	//console.log(timeLeft, sessionRunning, breakRunning);
-	//console.log("ready");
-	//these four click handlers are for setting the session and break timer
+	var clockRunning = false; // only change the session and break times when the clock is not running
+	var interval = null; // variable for setInterval() function
+
+	//these four click handlers are for setting the session and break time
 	$(".break").find(".plus").click(function(){
-		console.log("Bplus clicked");
-		var text = parseInt($("#breakText").text());
-		if(text <= 1440)
-			text = text + 1;
-		$("#breakText").html(text);
+		if(!clockRunning){
+			//console.log("Bplus clicked");
+			var text = parseInt($("#breakText").text());
+			if(text <= 1440)
+				text = text + 1;
+			$("#breakText").html(text);
+		}
 	});
 	$(".break").find(".minus").click(function(){
-		console.log("Bminus clicked");
-		var text = parseInt($("#breakText").text());
-		if(text > 1)
-			text = text - 1;
-		$("#breakText").html(text);
+		if(!clockRunning){
+			//console.log("Bminus clicked");
+			var text = parseInt($("#breakText").text());
+			if(text > 1)
+				text = text - 1;
+			$("#breakText").html(text);
+		}
 	});
 	$(".session").find(".plus").click(function(){
-		console.log("Splus clicked");
-		var text = parseInt($("#sessionText").text());
-		if(text <= 1440)
-			text = text + 1;
-		$("#sessionText").html(text);
+		if(!clockRunning){
+			//console.log("Splus clicked");
+			var text = parseInt($("#sessionText").text());
+			if(text <= 1440)
+				text = text + 1;
+			$("#sessionText").html(text);
+			timeLeft = text * 60;
+		}
 	});
 	$(".session").find(".minus").click(function(){
-		console.log("Sminus clicked");
-		var text = parseInt($("#sessionText").text());
-		if(text > 1)
-			text = text - 1;
-		$("#sessionText").html(text);
+		if(!clockRunning){
+			//console.log("Sminus clicked");
+			var text = parseInt($("#sessionText").text());
+			if(text > 1)
+				text = text - 1;
+			$("#sessionText").html(text);
+			timeLeft = text * 60;
+		}
 	});
 
 	//click handler for clock
 	$(".clock").click(function(){
-		var interval = setInterval(function(){ update(); }, 1000);
+		if(!clockRunning){
+			interval = setInterval(function(){ update(); }, 1000);
+			clockRunning = true;
+		}
+		else{
+			clockRunning = false;
+			clearInterval(interval);
+		}
 	});
 	
-	//for running the timer
+	//for running and updating the timer, switching between session and break times, and displaying the time
 	function update(){
+		//switch from session to break
 		if(timeLeft == 0 && sessionRunning == true){
 			sessionRunning = false;
 			breakRunning = true;
 			timeLeft = parseInt($("#breakText").text()) * 60;
 			$("#clockText").html("Break");
 		}
+		//switch from break to switch
 		else if(timeLeft == 0 && breakRunning == true){
 			breakRunning = false;
 			sessionRunning = true;
 			timeLeft = parseInt($("#sessionText").text()) * 60;
 			$("#clockText").html("Session");	
 		}
-		var hours = parseInt(timeLeft/3600); console.log(hours);
-		var minutes = parseInt( (timeLeft - hours * 3600) / 60 ); console.log(minutes);
-		var seconds = parseInt(  timeLeft - hours * 3600 - minutes * 60); console.log(seconds);
+		//calculating hr, min and seconds
+		var hours = parseInt(timeLeft/3600); 
+		var minutes = parseInt( (timeLeft - hours * 3600) / 60 ); 
+		var seconds = parseInt(  timeLeft - hours * 3600 - minutes * 60); 
+		//console.log(hours, minutes, seconds);
 		if (hours < 10){
 			var H = "0" + hours;
 		}
@@ -79,6 +101,4 @@ $(document).ready(function(){
 		$("#clockTextTime").html(clockTime);
 		timeLeft = timeLeft - 1;
 	}
-
-
 });
